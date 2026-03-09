6f7ab490eaecaa2f76375e4a2882e2c90957a2eb
eraybabur <148173007+eraybabur@users.noreply.github.com>
2026-03-09 15:22:04 +0000

    Complete data quality checks and missing value analysis

--- queries.sql
+++ queries.sql
@@ -7,4 +7,17 @@ SELECT * FROM netflix_titles
 SELECT count(*) - count(distinct show_id); 
 FROM netflix_titles;
 
- 
+-- Count how many Movies and TV Shows are in the dataset
+ SELECT type, count(*) as total_titles
+from netflix_titles
+group by type;
+
+-- Result:
+-- Movies: 6131
+-- TV Shows: 2676
+
+-- Check missing values in important columns
+SELECT count(*) - count(director) as missing_directors,
+count(*) - count("cast") as  missing_cast,
+count(*) - count(country) as missing_countries
+FROM netflix_titles
